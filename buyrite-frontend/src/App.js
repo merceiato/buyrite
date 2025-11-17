1b4ac4330e9c30dc77ab6fe028cae0fa6e6dad6f
Ubuntu <ubuntu@ip-172-31-11-5.ap-southeast-2.compute.internal>
2025-11-17 14:41:16 +0000

    time to debug

--- buyrite-frontend/src/App.js
+++ buyrite-frontend/src/App.js
@@ -5,9 +5,9 @@ import Toast from './Toast'
 
 class App {
   constructor(){
-    this.name = "Haircuts"
+    this.name = "Buyrite"
     this.version = "1.0.0"
-    this.apiBase = 'http://localhost:3000'
+    this.apiBase = 'http://ec2-3-27-66-119.ap-southeast-2.compute.amazonaws.com:3000'
     this.rootEl = document.getElementById("root")
     this.version = "1.0.0"
   }
