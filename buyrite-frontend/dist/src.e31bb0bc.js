5f91d2cbc3f3e15de8a158db981657ec91585857
dedatekk <dedatekk@gmail.com>
2025-11-17 21:06:32 +1000

    main modified for prod (partially)

--- buyrite-frontend/dist/src.e31bb0bc.js
+++ buyrite-frontend/dist/src.e31bb0bc.js
@@ -17579,7 +17579,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39049" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42431" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
